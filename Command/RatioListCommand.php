0646a16ab1a0977d19bd8c32036901d3595c39aa
Philippe Le Van (@plv) <philippe.levan@kitpages.fr>
2013-07-04 09:49:59 +0200

    ratio list command

---
+++ Command/RatioListCommand.php
@@ -0,0 +1,37 @@
+<?php
+namespace Tbbc\MoneyBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Tbbc\MoneyBundle\Pair\PairManagerInterface;
+
+class RatioListCommand
+    extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('tbbc:money:ratio-list')
+            ->setHelp("The <info>tbbc:money:ratio-list</info> display list of registered ratio")
+            ->setDescription('display list of registered ratio')
+        ;
+
+
+    }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var PairManagerInterface $pairManager */
+        $pairManager = $this->getContainer()->get("tbbc_money.pair_manager");
+        $ratioList = $pairManager->getRatioList();
+        $output->writeln("Ratio list");
+        foreach ($ratioList as $currencyCode => $ratio) {
+            $output->writeln("$currencyCode;$ratio");
+        }
+    }
+
+
+
+}
--- Tests/Console/ConsoleTest.php
+++ Tests/Console/ConsoleTest.php
@@ -10,12 +10,20 @@ class ConsoleTest
     public function testRunSaveRatio()
     {
         $client = self::createClient();
-        $kernel = $client->getKernel();
 
         $output = $this->runCommand($client, "tbbc:money:save-ratio USD 1.265");
 
         /** @var PairManagerInterface $pairManager */
         $pairManager = $client->getContainer()->get("tbbc_money.pair_manager");
         $this->assertEquals(1.265, $pairManager->getRelativeRatio("EUR", "USD"));
+    }
+    public function testRunRatioList()
+    {
+        $client = self::createClient();
+        $output = $this->runCommand($client, "tbbc:money:save-ratio USD 1.265");
+
+        $output = $this->runCommand($client, "tbbc:money:ratio-list");
+
+        $this->assertEquals("Ratio list\nEUR;1\nUSD;1.265\n\n", $output);
     }
 }
