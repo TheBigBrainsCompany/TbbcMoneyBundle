16e0eae502b2a5a8c5224b411702a284475b9044
Hugues Maignol <hugues.maignol@kitpages.fr>
2015-04-23 11:09:32 +0200

    Refactored RatioProviderTest for limiting code duplication and ease of test for additional RatioProviders

---
+++ Tests/Pair/RatioProvider/AbstractRatioProviderTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tbbc\MoneyBundle\Tests\Pair\Storage;
+
+use Tbbc\MoneyBundle\Pair\RatioProviderInterface;
+
+/**
+ * This class can be used to easily test your custom ratio providers.
+ *
+ * @author Hugues Maignol <hugues.maignol@kitpages.fr>
+ */
+abstract class AbstractRatioProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * The currently tested RatioProvider.
+     *
+     * @var RatioProviderInterface
+     */
+    protected $ratioProvider;
+
+    public function setUp()
+    {
+        $this->ratioProvider = $this->getRatioProvider();
+    }
+
+    public function testRatioFetching()
+    {
+        foreach ($this->getRatiosToTest() as $testParameters) {
+            $ratio = $this->ratioProvider->fetchRatio($testParameters['reference'], $testParameters['currency']);
+            $this->assertInternalType('float', $ratio, 'The fetched ratio must be a float');
+            $this->assertLessThan(
+                $testParameters['ratio_max'],
+                $ratio,
+                'The ratio is too high, are wee in deep economical crisis ?'
+            );
+            $this->assertGreaterThan(
+                $testParameters['ratio_min'],
+                $ratio,
+                'The ratio is too low'
+            );
+        }
+    }
+
+    public function testExceptionForUnknownCurrency()
+    {
+        $this->setExpectedException('Tbbc\MoneyBundle\MoneyException');
+        $this->ratioProvider->fetchRatio('ZZZ', 'USD');
+    }
+
+    /**
+     * Returns the instanciated RatioProvider service that will be tested.
+     *
+     * @return RatioProviderInterface
+     */
+    abstract protected function getRatioProvider();
+
+    /**
+     * Each array value returned is an array with the keys :
+     *  - reference : The base currency for the ratio
+     *  - currency : The currency for which we want the ratio
+     *  - ratio_min : The minimum ratio value considered valid
+     *  - ratio_max : The maximum ratio value considered valid.
+     *
+     * @return array[]
+     */
+    protected function getRatiosToTest()
+    {
+        return [
+            [
+                'reference' => 'EUR',
+                'currency' => 'USD',
+                'ratio_min' => 0.3,
+                'ratio_max' => 3,
+            ],
+            [
+                'reference' => 'GBP',
+                'currency' => 'EUR',
+                'ratio_min' => 0.3,
+                'ratio_max' => 3,
+            ],
+        ];
+    }
+}
--- Tests/Pair/RatioProvider/GoogleRatioProviderTest.php
+++ Tests/Pair/RatioProvider/GoogleRatioProviderTest.php
@@ -1,35 +1,20 @@
 <?php
+
 namespace Tbbc\MoneyBundle\Tests\Pair\Storage;
 
 use Tbbc\MoneyBundle\Pair\RatioProvider\GoogleRatioProvider;
 
 /**
  * @author Hugues Maignol <hugues.maignol@kitpages.fr>
- * @group manager
+ * @group  manager
  */
-class GoogleRatioProviderTest extends \PHPUnit_Framework_TestCase
+class GoogleRatioProviderTest extends AbstractRatioProviderTest
 {
-    /** @var  GoogleRatioProvider */
-    protected $ratioProvider;
-
-    public function setUp()
+    /**
+     * @inheritdoc
+     */
+    protected function getRatioProvider()
     {
-        $this->ratioProvider = new GoogleRatioProvider();
-    }
-
-    public function testRetriveRatio()
-    {
-        $ratio = $this->ratioProvider->fetchRatio("EUR", "USD");
-        $this->assertTrue(is_float($ratio));
-        // note : I assume ratio between EUR and USD will remain between 0.3 and 3.
-        // I believe that if it is not the case, this failing test won't be our biggest problem :-)
-        $this->assertTrue($ratio > 0.3);
-        $this->assertTrue($ratio < 3);
-    }
-
-    public function testException()
-    {
-        $this->setExpectedException('Tbbc\MoneyBundle\MoneyException');
-        $this->ratioProvider->fetchRatio("ZZZ", "USD");
+        return new GoogleRatioProvider();
     }
 }
