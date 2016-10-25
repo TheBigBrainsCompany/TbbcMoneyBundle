e9c7bfdcabe118e1bf21aed1aa597b07e442cfbb
Pavel Dubinin <geekdevs@gmail.com>
2016-10-25 17:50:32 +0300

    Fixed path for doctrine tests

--- Tests/BundleOrmTestCase.php
+++ Tests/BundleOrmTestCase.php
@@ -24,7 +24,7 @@ class BundleOrmTestCase
         $eventManager->addEventListener(array("preTestSetUp"), new SchemaSetupListener());
         
         $driver = new SimplifiedXmlDriver(array(
-            __DIR__ . '/../Resources/config/doctrine' => 'Tbbc\MoneyBundle\Entity'
+            __DIR__ . '/../Resources/config/doctrine/ratios' => 'Tbbc\MoneyBundle\Entity'
         ));
 
         // create config object
