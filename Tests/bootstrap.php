a0d6da2ff37eaa0d1178c97ad733f4aaa1478c23
Konstantin <konstantin.kuklin@gmail.com>
2018-09-08 13:58:32 +0300

    move autoload to psr-4 (#90)
    * move autoload to psr-4
    
    * fix AppKernel stub loading

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,3 +3,5 @@ if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
     throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
 require $autoloadFile;
+// we should include AppKernel stub here, because only tests should know about it class and nobody else
+require 'app/AppKernel.php';
