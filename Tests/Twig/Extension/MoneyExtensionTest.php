f3ba3b5932c56c50c1aea7d7ffe4a08781f7b6b9
Martin Aarhof <martin.aarhof@gmail.com>
2019-05-20 13:52:50 +0200

    Reverted whitespace changes

--- Tests/Twig/Extension/MoneyExtensionTest.php
+++ Tests/Twig/Extension/MoneyExtensionTest.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Tbbc\MoneyBundle\Tests\Twig\Extension;
-
 use Money\Currency;
 use Money\Money;
 use Tbbc\MoneyBundle\Formatter\MoneyFormatter;
@@ -19,7 +17,6 @@ class MoneyExtensionTest extends TestCase
      * @var MoneyExtension
      */
     private $extension;
-
     /**
      * @var array
      */
@@ -34,39 +31,34 @@ class MoneyExtensionTest extends TestCase
         $pairManager->expects($this->any())
             ->method('getReferenceCurrencyCode')
             ->will($this->returnValue("EUR"));
-
         $this->extension = new MoneyExtension(new MoneyFormatter(2), $pairManager);
         $this->variables = array('price' => new Money(123456789, new Currency('EUR')));
     }
-
     /**
      * @dataProvider getMoneyTests
      */
-    public function testMoney($template, $expected, $errorMessage)
+    public function testMoney($template, $expected)
     {
-        $this->assertSame($expected, $this->getTemplate($template)->render($this->variables), $errorMessage);
+        $this->assertEquals($expected, $this->getTemplate($template)->render($this->variables));
     }
-
     public function getMoneyTests()
     {
         return array(
-            array('{{ price|money_localized_format }}', '1 234 567,89 €', '1'),
-            array('{{ price|money_localized_format("en_US") }}', '€1,234,567.89', '2'),
-            array('{{ price|money_format }}', '1 234 567,89 €', '3'),
-            array('{{ price|money_format(".", ",") }}', '1,234,567.89 €', '4'),
-            array('{{ price|money_format_amount }}', '1 234 567,89', '5'),
-            array('{{ price|money_format_amount(".", ",") }}', '1,234,567.89', '6'),
-            array('{{ price|money_format_currency }}', '€', '7'),
-            array('{{ price|money_as_float }}', '1234567.89', '8'),
+            array('{{ price|money_localized_format }}', '1 234 567,89 €'),
+            array('{{ price|money_localized_format("en_US") }}', '€1,234,567.89'),
+            array('{{ price|money_format }}', '1 234 567,89 €'),
+            array('{{ price|money_format(".", ",") }}', '1,234,567.89 €'),
+            array('{{ price|money_format_amount }}', '1 234 567,89'),
+            array('{{ price|money_format_amount(".", ",") }}', '1,234,567.89'),
+            array('{{ price|money_format_currency }}', '€'),
+            array('{{ price|money_as_float }}', '1234567.89'),
         );
     }
-
     protected function getTemplate($template)
     {
         $loader = new ArrayLoader(array('index' => $template));
         $twig = new Environment($loader, array('debug' => true, 'cache' => false));
         $twig->addExtension($this->extension);
-
         return $twig->loadTemplate('index');
     }
 }
