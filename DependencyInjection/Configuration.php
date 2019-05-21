6858d8c95003fbdd19b8234f5f22b36b66e0ec54
Martin Aarhof <martin.aarhof@gmail.com>
2019-05-21 07:20:00 +0200

    Added root name to treebuilder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tbbc_money');
+        $treeBuilder = new TreeBuilder('tbbc_money');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('tbbc_money');
+        }
+
         $this->addCurrencySection($rootNode);
 
         return $treeBuilder;
