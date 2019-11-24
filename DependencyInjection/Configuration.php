817b2ed5e31172e04ecf9cf7a4498562a7d23690
Murat Özkorkmaz <murat.oezkorkmaz@gmail.com>
2019-11-24 22:04:06 +0100

    Fixed: Deprecated tree builders without root nodes
    See https://symfony.com/blog/new-in-symfony-4-2-important-deprecations#deprecated-tree-builders-without-root-nodes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tbbc_money');
+        $treeBuilder = new TreeBuilder('tbbc_money');
+        $rootNode = $treeBuilder->getRootNode();
         $this->addCurrencySection($rootNode);
 
         return $treeBuilder;
