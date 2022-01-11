42ac528121d223df200f4aa0c8e5415762bd5ed6
Johan Wilfer <johan@jttech.se>
2022-01-11 22:33:28 +0100

    Reverse order

--- Pair/PairManager.php
+++ Pair/PairManager.php
@@ -103,7 +103,7 @@ class PairManager implements PairManagerInterface, Exchange
             $ratio,
             $savedAt
         );
-        $this->dispatcher->dispatch(TbbcMoneyEvents::AFTER_RATIO_SAVE, $event);
+        $this->dispatcher->dispatch($event, TbbcMoneyEvents::AFTER_RATIO_SAVE);
     }
 
     /**
