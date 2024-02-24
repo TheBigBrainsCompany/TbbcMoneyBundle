eb7e7ceecc4ac5a97e6656e672921961d421904c
Christopher Georg <christopher.georg@sr-travel.de>
2024-02-24 20:55:36 +0100

    feat: allow PHPUnit 11

---
+++ Tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+use Symfony\Component\ErrorHandler\ErrorHandler;
+
+# PHPUnit 11 needs this, otherwise there is an error 'Test code or tested code did not remove its own exception handlers'
+ErrorHandler::register(null, false);
