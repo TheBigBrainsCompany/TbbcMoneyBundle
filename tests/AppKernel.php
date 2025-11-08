863412f3b4e578e58b22201285d373a50e2ca65d
Christopher Georg <christopher.georg@sr-travel.de>
2025-11-08 10:39:42 +0100

    chore: allow doctrine/doctrine-bundle 3 bump min version to doctrine/doctrine-bundle 2.13.3

--- tests/AppKernel.php
+++ tests/AppKernel.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace Tbbc\MoneyBundle\Tests;
 
-use Composer\InstalledVersions;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 use Tbbc\MoneyBundle\TbbcMoneyBundle;
 
@@ -34,35 +32,6 @@ class AppKernel extends Kernel
             yield new DoctrineMongoDBBundle(),
             yield new TbbcMoneyBundle(),
         ];
-    }
-
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
-    {
-        $container->loadFromExtension('framework');
-
-        # doctrine-bundle
-        if (null !== $doctrineBundleVersion = InstalledVersions::getVersion('doctrine/doctrine-bundle')) {
-
-            # v2
-            if (version_compare($doctrineBundleVersion, '3.0.0', '<')) {
-                $orm['auto_generate_proxy_classes'] = true;
-                $orm['use_savepoints'] = true;
-                $orm['report_fields_where_declared'] = true;
-                $orm['auto_mapping']['controller_resolver'] = true;
-            }
-
-            if (version_compare($doctrineBundleVersion, '2.8.0', '>=')) {
-                $orm['enable_lazy_ghost_objects'] = true;
-            }
-
-            if (\PHP_VERSION_ID >= 80400 && version_compare($doctrineBundleVersion, '2.15.0', '>=')) {
-                $orm['enable_native_lazy_objects'] = true;
-            }
-        }
-
-        $container->prependExtensionConfig('doctrine', [
-            'orm' => $orm,
-        ]);
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader): void
