2fa27fb94f6e6d66cb4e6ca4832e61d34b8d3d4e
Johan Wilfer <johan@sirvoy.com>
2025-11-30 20:15:46 +0100

    format cs

--- src/Resources/config/form_types.php
+++ src/Resources/config/form_types.php
@@ -6,17 +6,26 @@ use Tbbc\MoneyBundle\Form\Type\{CurrencyType, MoneyType, SimpleMoneyType};
 return static function (ContainerConfigurator $configurator): void {
     $services = $configurator->services();
 
-    $services->set(CurrencyType::class)->arg('$currencyCodeList', '%tbbc_money.currencies%')->arg('$referenceCurrencyCode', '%tbbc_money.reference_currency%')
+    $services
+        ->set(CurrencyType::class)
+        ->arg('$currencyCodeList', '%tbbc_money.currencies%')
+        ->arg('$referenceCurrencyCode', '%tbbc_money.reference_currency%')
         ->tag('form.type', [
             'alias' => 'tbbc_currency',
         ]);
 
-    $services->set(MoneyType::class)->arg('$decimals', '%tbbc_money.decimals%')
+    $services
+        ->set(MoneyType::class)
+        ->arg('$decimals', '%tbbc_money.decimals%')
         ->tag('form.type', [
             'alias' => 'tbbc_money',
         ]);
 
-    $services->set(SimpleMoneyType::class)->arg('$decimals', '%tbbc_money.decimals%')->arg('$currencyCodeList', '%tbbc_money.currencies%')->arg('$referenceCurrencyCode', '%tbbc_money.reference_currency%')
+    $services
+        ->set(SimpleMoneyType::class)
+        ->arg('$decimals', '%tbbc_money.decimals%')
+        ->arg('$currencyCodeList', '%tbbc_money.currencies%')
+        ->arg('$referenceCurrencyCode', '%tbbc_money.reference_currency%')
         ->tag('form.type', [
             'alias' => 'tbbc_simple_money',
         ]);
